38eb853ba48271281ee67afbb0e97e89bb6a3efb
Obodo Bright <obodobright0@gmail.com>
2024-01-07 18:45:39 +0100

    Table and Filter: Create dashboard table and filters for the dashboard static page

--- src/components/tags/index.tsx
+++ src/components/tags/index.tsx
@@ -1,10 +1,10 @@
 import styles from "./tags.module.scss";
 
-enum UserStatus {
-    Inactive = "inactive",
-    Active = "active",
-    Blacklisted = "blacklisted",
-    Pending = "pending",
+enum ItemStatus {
+    Reconcilled = "Reconcilled",
+    Unreconcilled = "Un-reconcilled",
+    Pending = "Pending",
+    Settled = "Settled",
 }
 
 
@@ -15,16 +15,19 @@ interface tagsProp {
 
 const Tags = ({ status }: tagsProp) => {
 
-    const displayStyle = status === UserStatus.Inactive ?
-        { background: "#f5f5f7", color: "#545F7D" }
-        : status === UserStatus.Active
-            ? { background: "#f3fcf6", color: "#39CD62" }
-            : status === UserStatus.Pending ?
-                { background: "#fdf7e5", color: "#E9B200" }
-                : { background: "#fce6eb", color: "#E4033B" }
+    const displayStyle = status === ItemStatus.Reconcilled ?
+        { color: "#27AE60" }
+        : status === ItemStatus.Unreconcilled
+            ? { color: "#C4C4C4" }
+            : status === ItemStatus.Pending ?
+                { color: "#EBC315" }
+                : { color: "#E4033B" }
 
     return (
-        <div style={displayStyle} className={styles.tagsWrapper}>{status}</div>
+        <div style={displayStyle} className={styles.tagsWrapper}>
+            <div className={styles.statusIndicator}></div>
+            <span>{status}</span>
+        </div>
     )
 }
 export default Tags
